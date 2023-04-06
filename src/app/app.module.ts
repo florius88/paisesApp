b940a73a2fe1ff550c84269b1ce6e6638a09f379
florius88 <florsantosbueno@gmail.com>
2023-04-06 20:22:47 +0200

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 import { PaisModule } from './pais/pais.module';
 import { SharedModule } from './shared/shared.module';
+
 
 
 @NgModule({
@@ -13,6 +15,7 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     PaisModule,
     SharedModule
   ],
