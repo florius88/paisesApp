234d5fc01c9089c4aa87e8422dd7afa93522650d
florius88 <florsantosbueno@gmail.com>
2023-04-07 14:27:41 +0200

    Control de Errores y Tipado del servicio

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { Country } from '../interfaces/pais.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -12,14 +13,12 @@ export class PaisService {
 
   constructor(private http: HttpClient) { }
 
-  buscarPais(termino: string): Observable<any> {
+  buscarPais(termino: string): Observable<Country[]> {
 
     const url = `${this.apiUrl}/name/${termino}`;
 
-    return this.http.get(url)
-      /* .pipe(
-        catchError(err => of(['Error']))
-      ) */
+    return this.http.get<Country[]>(url)
+      /* .pipe(catchError(err => of(['Error']))) */
       ;
   }
 
