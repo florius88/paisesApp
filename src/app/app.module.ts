73a450782d5acb788370099cdf7b8c91d6eb6c46
florius88 <florsantosbueno@gmail.com>
2023-04-07 14:00:00 +0200

    Servicio Buscar Pais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-
 import { PaisModule } from './pais/pais.module';
 import { SharedModule } from './shared/shared.module';
+
+import { AppRoutingModule } from './app-routing.module';
 
 
 
@@ -16,6 +17,7 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     PaisModule,
     SharedModule
   ],
