1bb48fcf67e8499bd21230f3b203169df0a8b304
florius88 <florsantosbueno@gmail.com>
2023-04-11 17:01:20 +0200

    Buscar por regiones

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-region',
@@ -13,20 +15,33 @@ import { Component } from '@angular/core';
 })
 export class PorRegionComponent {
 
-  regiones: string[] = ['asia', 'áfrica', 'américa del norte', 'américa del sur',
-    'antártida', 'europa', 'oceanía'];
+  regiones: string[] = ['asia', 'africa', 'americas', 'antarctic', 'europe', 'oceania'];
   regionActiva: string = '';
+  paises: Country[] = [];
 
-  constructor() { }
+  constructor(private paisService: PaisService) { }
 
   getClaseCss(region: string) {
-    return (region === this.regionActiva) ? 'btn btn-primary': 'btn btn-outline-primary';
+    return (region === this.regionActiva) ? 'btn btn-primary' : 'btn btn-outline-primary';
   }
 
   activarRegion(region: string) {
-    this.regionActiva = region;
 
-    // TODO: hacer el llamado al servicio
+    if (region === this.regionActiva) { return }
+
+    this.regionActiva = region;
+    this.paises = [];
+
+    this.paisService.buscarRegion(this.regionActiva)
+      .subscribe({
+        next: (resp) => {
+          console.log(resp)
+          this.paises = resp;
+        },
+        error: (err) => {
+          this.paises = [];
+        }
+      });
 
   }
 
--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -21,18 +21,18 @@ export class PaisService {
       ;
   }
 
-  buscarCapital(termino:string): Observable<Country[]> {
+  buscarCapital(termino: string): Observable<Country[]> {
     const url = `${this.apiUrl}/capital/${termino}`;
     return this.http.get<Country[]>(url);
   }
 
-  getPaisPorAlpha( id:string ): Observable<Country[]> {
+  getPaisPorAlpha(id: string): Observable<Country[]> {
     const url = `${this.apiUrl}/alpha/${id}`;
     return this.http.get<Country[]>(url);
   }
 
-  buscarRegion(termino:string): Observable<Country[]> {
-    const url = `${this.apiUrl}/region/${termino}`;
+  buscarRegion(region: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/region/${region}`;
     return this.http.get<Country[]>(url);
   }
 
