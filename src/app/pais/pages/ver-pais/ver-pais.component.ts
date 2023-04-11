7e49018f6140a967b8c787446a94bf76cac45c72
florius88 <florsantosbueno@gmail.com>
2023-04-11 13:54:06 +0200

    SwitchMap

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -15,16 +17,15 @@ export class VerPaisComponent implements OnInit {
   }
 
   ngOnInit(): void {
+
     this.activatedRoute.params
-      .subscribe(({ id }) => {
-        console.log(id);
+      .pipe(
+        switchMap( ({ id }) => this.paisService.getPaisPorAlpha(id))
+      )
+      .subscribe(resp => {
+        console.log(resp);
+      } )
 
-        this.paisService.getPaisPorAlpha(id)
-          .subscribe(pais => {
-            console.log(pais);
-          })
-      })
   }
-
 
 }
