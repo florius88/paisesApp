32fe908921ed780f1c3ec6bf8a4be96b3e998d30
florius88 <florsantosbueno@gmail.com>
2023-04-11 13:31:49 +0200

    Por Capital

--- src/app/pais/pages/por-capital/por-capital.component.ts
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
+
+
 @Component({
   selector: 'app-por-capital',
   templateUrl: './por-capital.component.html',
@@ -7,5 +11,30 @@ import { Component } from '@angular/core';
   ]
 })
 export class PorCapitalComponent {
+
+  termino: string = '';
+  hayError: boolean = false;
+  paises: Country[] = [];
+
+  constructor(private paisService: PaisService) { }
+
+  buscar(termino: string) {
+    this.hayError = false;
+    this.termino = termino;
+
+    console.log(this.termino);
+
+    this.paisService.buscarCapital(this.termino)
+      .subscribe({
+        next: (resp) => {
+          console.log(resp)
+          this.paises = resp;
+        },
+        error: (err) => {
+          this.hayError = true
+          this.paises = [];
+        }
+      });
+  }
 
 }
--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+
 import { Country } from '../interfaces/pais.interface';
 
 @Injectable({
@@ -16,6 +17,15 @@ export class PaisService {
   buscarPais(termino: string): Observable<Country[]> {
 
     const url = `${this.apiUrl}/name/${termino}`;
+
+    return this.http.get<Country[]>(url)
+      /* .pipe(catchError(err => of(['Error']))) */
+      ;
+  }
+
+  buscarCapital(termino:string): Observable<Country[]> {
+
+    const url = `${this.apiUrl}/capital/${termino}`;
 
     return this.http.get<Country[]>(url)
       /* .pipe(catchError(err => of(['Error']))) */
